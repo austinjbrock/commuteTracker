349ef67a87a9f169df9b7f255a8501b6bd683198
Austin Brock <austinjbrock@gmail.com>
2026-07-14 07:13:39 -0700

    Add welcome message and edited sout text

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String [] args){
+        System.out.println("Welcome To Commute Tracker");
     CommuteTracker tracker = new CommuteTracker();
     //Utilize Scanner so we can get user input
     Scanner scanner = new Scanner(System.in);
     String answer = "y";
         while (answer.equals("y")) {
             // grab user input so we can track minutes and update totalMinutes variable
-            System.out.println("Enter commute minutes");
+            System.out.println("Enter commute minutes below");
             int minutes = scanner.nextInt();
 
             System.out.println("What day was this commute?");
